2d6d31919c8b923d3d2e6443d2daa34ceaa2a4dd
Diego Passos Silva <Diego_Passos2@hotmail.com>
2021-10-28 10:42:59 -0300

    criação de tela de login e home

--- tshield-teste-angular/src/app/app-routing.module.ts
+++ tshield-teste-angular/src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FullLayoutComponent } from './containers/full-layout';
+import { SimpleLayoutComponent } from './containers/simple-layout';
+import { HomeComponent } from './view/home/home.component';
+import { LoginComponent } from './view/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: SimpleLayoutComponent, data: { title: 'Login' },
+    children: [
+      { path: '', component: LoginComponent }
+    ]
+  },
+  {
+    path: 'login', component: SimpleLayoutComponent, data: { title: 'Login' },
+    children: [
+      { path: '', component: LoginComponent }
+    ]
+  },
+  {
+    path: 'home', component: FullLayoutComponent, data: { title: 'Home' },
+    children: [
+      { path: '', component: HomeComponent }
+    ]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled', relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- tshield-teste-angular/src/app/app.module.ts
+++ tshield-teste-angular/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmptyLayoutComponent } from './containers/empty-layout/empty-layout.component';
+import { FullLayoutComponent } from './containers/full-layout/full-layout.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HeaderComponent } from './component/header/header.component';
+import { LoginComponent } from './view/login/login.component';
+import { SimpleLayoutComponent } from './containers/simple-layout/simple-layout.component';
+import { HomeComponent } from './view/home/home.component';
+
+const APP_CONTAINERS = [
+  SimpleLayoutComponent,
+  FullLayoutComponent,
+  EmptyLayoutComponent
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ...APP_CONTAINERS,
+    FooterComponent,
+    HeaderComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
